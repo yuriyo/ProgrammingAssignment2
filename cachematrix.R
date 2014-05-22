110d92c8695fc98bd092636b5eabf917a978dbdf
Yuriy Osadchyy <yuriyo@yuriyonote.local>
2014-05-22 23:53:34 +0300

    assignment done. testing done

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
+##
+## Cache for time-consuming operation of matrix inverse
+##
+## Coursera DataScience course
+## R Programming class
+## Assignment #2
+## @a : yuriyo
+##
+## @note : "For this assignment, assume that the matrix supplied is always invertible"
 
-## Write a short comment describing this function
-
+#
+## build matrix object that can store inverse for it
+#
 makeCacheMatrix <- function(x = matrix()) {
-
+  m_inverse <- NULL
+  m_data <- x
+  
+  get <- function() m_data
+  set <- function(x) {
+    m_data <<- x
+    m_inverse <<- NULL
+  }
+  
+  getinverse <- function() m_inverse
+  setinverse <- function(inverse) m_inverse <<- inverse
+    
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+#
+## Calculate inverse of wrapped matrix object
+#
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  #
+  # get the cached value
+  #
+  inverse <- x$getinverse()
+  #
+  # that would be empty not calculated yet
+  #
+  if(!is.null(inverse)) {
+    return(inverse)
+  }
+  #
+  # calculate inverse
+  #
+  inverse <- solve(x$get(), ...)
+  #
+  # push it as cache value
+  #
+  x$setinverse(inverse)
+  #
+  # result
+  #
+  inverse
 }
